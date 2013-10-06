3c651fe3cb6d01540d27bf39af2e6d90f0cd469f
Dowser <dowser@hotmail.com>
2013-10-06 23:49:19 +0200

    Update fileflyer.php

--- fileflyer.php
+++ fileflyer.php
@@ -56,7 +56,7 @@ Class Fileflyer {
 		$cookietime = time() + 100;
 		$useragent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.76 Safari/537.36"; // The Useragent
 		
-        $ch = curl_init();
+		$ch = curl_init();
                 curl_setopt($ch,CURLOPT_URL,$url);
                 curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
